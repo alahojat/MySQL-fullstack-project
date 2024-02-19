72b27c20aecb52d7b0f2af755dacc7b20293d12a
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-02-19 18:51:07 +0100

    ✨ feat(users.js) add get and post endpoints for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const { randomUUID } = require('crypto');
 
 const CryptoJS = require('crypto-js');
 
-/* GET users listing. */
+// Get all users
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  connection.connect((err) => {
+    if (err) {
+      console.log("err", err);
+    }
+    let sql = "SELECT * FROM users";
+
+    connection.query(sql, (err, data) => {
+      if (err) console.log("err", data);
+      console.log("to do note", data);
+      res.json(data);
+    })
+  })
+});
+
+
+// add a new user
+router.post('/', function(req, res, next) {
+
+  let userName = req.body.userName;
+  let userEmail = req.body.userEmail;
+  let userPassword = req.body.userPassword;
+  let uuid = randomUUID();
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("err", err);
+    }
+
+    let sql = "INSERT into users (uuid, userName, userEmail, userPassword) VALUES (?, ?, ?, ?)";
+    let values = [uuid, userName, userEmail, userPassword];
+
+    connection.query(sql, values, (err, data) => {
+      if (err) console.log("err", data);
+
+      console.log("new user", data);
+      res.json({message: "Your new user is added"});
+    })
+  })
 });
 
 module.exports = router;
